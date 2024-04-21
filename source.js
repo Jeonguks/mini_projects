d6439756fdac217be3fcbbb17bc06fda3911aea0
Jeonguks <ju460648@gmail.com>
2024-04-22 03:38:29 +0900

    Test1

--- source.js
+++ source.js
@@ -3,6 +3,10 @@ var ball =0;
 var strike =0;
 var out = 0;
 var j=1;
+var startTime;
+var timerInterval;
+var seconds;
+window.onload=hideInput();
 
 function generateNumber(){
     var answer =[];
@@ -35,7 +39,9 @@ function checkValue(inputValue){
         }
     }
     if(strike==4){
-        alert("정답입니다!");
+        alert("정답입니다! \nClaer Time: "+seconds+"초");
+        stopTimer();
+        location.reload();
     }
     else if(j>14&&strike!=4){
         alert("GAME OVER!");
@@ -53,9 +59,40 @@ function checkValue(inputValue){
     out=0;
     j++
 }
+function hideInput(){
+    let inputNumberDiv = document.querySelector(".inputNumber");
+    let listDiv = document.querySelector(".list");
+    inputNumberDiv.style.visibility ='hidden'
+    listDiv.style.visibility ='hidden'
+}
+function gameStart(){
+    let inputNumberDiv = document.querySelector(".inputNumber");
+    let gamestartDiv = document.querySelector(".gamestart");
+    let listDiv = document.querySelector(".list");
+    inputNumberDiv.style.visibility ='visible'
+    listDiv.style.visibility ='visible'
+    gamestartDiv.style.display ='none';
+    startTimer();
+}
+function startTimer() {
+    startTime = new Date(); // 시작 시간 기록
+    timerInterval = setInterval(updateTimer, 1000); // 1초마다 updateTimer 함수 호출
+}
+function updateTimer() {
+    var currentTime = new Date(); // 현재 시간 기록
+    var elapsedTime = currentTime - startTime; // 경과 시간 계산 (밀리초 단위)
+
+    // 밀리초를 초 단위로 변환하여 출력
+    seconds = Math.floor(elapsedTime / 1000);
+    console.log("경과 시간:", seconds, "초");
+}
+function stopTimer() {
+    clearInterval(timerInterval); // 타이머 정지
+    console.log("타이머가 정지되었습니다.");
+}
 document.querySelector("#btnInput").onclick = function (){
     var inputValue = document.querySelector("#numInput").value;
-    if(inputValue ===""||parseInt(inputValue)<1){
+    if(parseInt(inputValue)<1||isNaN(inputValue)){
         alert("입력값이 올바르지 않습니다!")
     } 
     else if(inputValue.length!=4){
